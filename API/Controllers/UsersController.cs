c312ae1cae5ceb841fe8d066a0a41ae760db59da
peshovskim <peshovskiimartin@gmail.com>
2020-11-17 03:08:17 +0100

    ReverseTest

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -25,7 +25,7 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
 
-        // test/users/id
+        // api/users/id
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
